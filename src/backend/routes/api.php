55459f673179eaae9f84872ce41cbe293f7c1930
Valeriy Bakalenko <valeriy.bakalenko@gmail.com>
2021-03-23 17:03:41 +0300

    Api Routes

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
