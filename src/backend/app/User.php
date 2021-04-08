dba54a081f0684f4e4b48a1217856ccd768afb29
Valeriy Bakalenko <valeriy.bakalenko@gmail.com>
2021-04-08 21:21:55 +0300

    add: soft deletes for models

--- src/backend/app/User.php
+++ src/backend/app/User.php
@@ -3,13 +3,16 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     protected $table = 'users';
 
