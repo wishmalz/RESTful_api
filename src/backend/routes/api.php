c3705905947e953db59feb4db97ecd357ad97bf1
Valeriy Bakalenko <valeriy.bakalenko@gmail.com>
2021-04-09 10:45:23 +0300

    add: transactions and buyer controllers

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -14,10 +14,18 @@ use Illuminate\Http\Request;
 */
 
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
+
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
+
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
