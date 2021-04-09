2411fd73a79fcbc8972976fe4095b2b40acfd941
Valeriy Bakalenko <valeriy.bakalenko@gmail.com>
2021-04-09 19:01:47 +0300

    add: cache system

--- src/backend/app/Traits/ApiResponser.php
+++ src/backend/app/Traits/ApiResponser.php
@@ -4,6 +4,9 @@ namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponser
 {
@@ -40,7 +43,9 @@ trait ApiResponser
         $transformer = $collection->first()->transformer;
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse($collection, $code);
     }
@@ -81,6 +86,11 @@ trait ApiResponser
         return $transformation->toArray();
     }
 
+    /**
+     * @param Collection $collection
+     * @param $transformer
+     * @return Collection|mixed
+     */
     protected function sortData(Collection $collection, $transformer)
     {
         if (request()->has('sort_by')) {
@@ -92,6 +102,11 @@ trait ApiResponser
         return $collection;
     }
 
+    /**
+     * @param Collection $collection
+     * @param $transformer
+     * @return Collection
+     */
     protected function filterData(Collection $collection, $transformer)
     {
         foreach (request()->query() as $query => $value) {
@@ -103,5 +118,51 @@ trait ApiResponser
         }
 
         return $collection;
+    }
+
+    /**
+     * @param Collection $collection
+     * @return LengthAwarePaginator
+     */
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30/60, function () use ($data) {
+            return $data;
+        });
     }
 }
